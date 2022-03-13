53f386871da850a6bbc217956a599eb650318df6
Shivam Kushwaha <shivamk8368@gmail.com>
2022-03-13 13:17:39 -0500

    Fourth commit - updated contact info

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@
 
 		//Create a countdown instance. Change the launchDay according to your needs.
 		//The month ranges from 0 to 11. I specify the month from 1 to 12 and manually subtract the 1.
-		var launchDay = new Date(2023, 12-1, 25);
+		var launchDay = new Date(2022, 5-1, 15);
 		
 		$("#timer").countdown({
 			until:launchDay
